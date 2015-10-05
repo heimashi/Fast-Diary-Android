bafb909acdd3dfaab5d5e383c5f8a6166e6d57da
heimashi <heimashi0728@126.com>
2015-10-05 10:52:34 +0800

    update

--- EasyDiary/app/src/main/java/cn/swang/ui/activity/SettingsActivity.java
+++ EasyDiary/app/src/main/java/cn/swang/ui/activity/SettingsActivity.java
@@ -14,33 +14,19 @@ import cn.swang.ui.base.BaseActivity;
  */
 public class SettingsActivity extends BaseActivity {
 
-    private EditText mEditText;
-    private TextInputLayout mTextInputLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        mTextInputLayout = (TextInputLayout)findViewById(R.id.feedback_text_input_layout);
-        mTextInputLayout.setHint(getString(R.string.feedback_tint));
-        mEditText=(EditText)findViewById(R.id.feedback_edit_text);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_feedback, menu);
-        return true;
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                finish();
-                return true;
-            case R.id.id_feedback_send:
-                //
-
                 finish();
                 return true;
         }
